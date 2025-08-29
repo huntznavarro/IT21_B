e9096c0186607be32be019a4e62c2cfb5079f54a
huntznavarro <huntzorravan@gmail.com>
2025-08-29 08:55:31 +0800

    added initial arithmetic

--- hello.js
+++ hello.js
@@ -4,5 +4,15 @@ num1= 2;
 num2= 3;
 
 sum = num1 + num2;
+diff = num1 - num2;
+ prod = num1 * num2;
+qou = num1 / num2;
 
-console.log("the sum of" + " " + num1 +""+ num2 + "is" + "="  + sum);
+
+console.log("The sum of "+ num1 + " & " + num2+ " is " + sum  );
+
+console.log("The diffrencre of "+ num1 + " & "+ num2+ " is " + diff  );
+
+console.log("The product of "+ num1 + " & "+ num2+ " is " + prod  );
+
+console.log("The qoutient  of "+ num1 + " & "+ num2+ " is " + qou );
