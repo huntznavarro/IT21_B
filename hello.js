9b5fd2266c97e5e302f94c8e6d1dffb03185da49
huntznavarro <huntzorravan@gmail.com>
2025-09-07 20:11:09 +0800

    Add menu options with Hello and factorial

--- hello.js
+++ hello.js
@@ -12,3 +12,23 @@ console.log("1. Say Hello");
 console.log("2. Factorial");
 console.log("3. Exit Application");
 console.log("---------------------------");
+
+rl.question("Enter your choice (1-3): ", choice =>{
+    console.log(choice);
+
+    if (choice === "1"){
+        console.log("Hello");
+    } else if (choice === "2"){
+        console.log("Factorial");
+
+        rl.question("Please enter a number for factorial: ", numStr =>{
+            let num = parseInt(numStr);
+
+            if (isNaN(num) || num < 0){
+                console.log("Please enter a non-negative integer");
+            } else {
+                let fact = 1;
+
+                for (let i=1; i <= num; i++){
+                    fact *=i;
+                }
