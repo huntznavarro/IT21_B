64377ffa8793e5082fd0e28e1e86b8d41500627d
irish199718 <huntzorravan@gmail.com>
2025-09-28 13:25:20 +0800

    added new sum of all numbers even and odd identify

--- practice.js
+++ practice.js
@@ -1,36 +1,36 @@
+
 const readline = require("readline");
 
+// Create interface for input/output
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+// Main function: ask user for numbers, calculate sum, check odd/even
 function sumAndCheck() {
     rl.question("Enter numbers separated by spaces: ", input => {
-        // Convert input string into an array of numbers
-        let numbers = input.split(" ").map(num => parseInt(num));
+        // Split input into array and convert to numbers
+        let numbers = input.split(" ").map(Number);
 
-        // Validate numbers
-        if (numbers.some(isNaN)) {
-            console.log("Invalid input! Please enter only numbers.");
-            return sumAndCheck();
+        // Calculate sum of all numbers
+        let sum = 0;
+        for (let num of numbers) {
+            sum += num;
         }
-
-        // Calculate sum
-        let sum = numbers.reduce((acc, val) => acc + val, 0);
 
         console.log("The sum of numbers is:", sum);
 
-        // Check if sum is odd or even
+        // Check odd or even
         if (sum % 2 === 0) {
             console.log("The sum is Even.");
         } else {
             console.log("The sum is Odd.");
         }
 
-        rl.close();
+        rl.close(); // End program
     });
 }
 
+// Start the program
 sumAndCheck();
-
